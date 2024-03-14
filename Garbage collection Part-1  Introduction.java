f4e9665648c6fe316b9f1353e894141e7bc60be5
yadav Bakelal <bake2101992@gmail.com>
2024-03-14 10:51:31 +0530

    Add files via upload

---
+++ Garbage collection Part-1  Introduction.java
@@ -0,0 +1,22 @@
+
+											 	Garbage collection Part-1 || Introduction
+											================================================
+
+ ---------------------
+  Garbage Collection  
+ ---------------------
+
+	1. Introduction.
+	2. The ways to make an object eligible for GC.
+	3. The methods for requesting JVM to run garbage collector (GC).
+	4. Finalization.
+	
+	
+==========================================================================================================================
+
+-----------------
+  Introduction
+-----------------
+
+ ->   
+ 
